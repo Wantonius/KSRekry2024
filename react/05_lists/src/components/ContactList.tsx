52b2386cfbfbd64f3d58c1259bc57c35c3456ee4
Erno Hentonen <erno.hentonen@opiframe.com>
2024-06-11 12:31:47 +0300

    lists

---
+++ react/05_lists/src/components/ContactList.tsx
@@ -0,0 +1,41 @@
+import Contact from '../models/Contact';
+
+interface Props {
+	list:Contact[];
+	removeContact(id:number):void;
+}
+
+const ContactList = (props:Props) => {
+	
+	const contactJSX = props.list.map((contact) => {
+		return(
+			<tr key={contact.id}>
+				<td>{contact.firstname}</td>
+				<td>{contact.lastname}</td>
+				<td>{contact.email}</td>
+				<td>{contact.phone}</td>
+				<td><button onClick={() => props.removeContact(contact.id)} className="btn btn-danger">Remove</button></td>
+			</tr>
+		)
+	})
+	
+	return(
+		<table className="table table-striped">
+			<thead>
+				<tr>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Email</th>
+					<th>Phone</th>
+					<th>Remove</th>
+				</tr>
+			</thead>
+			<tbody>
+			{contactJSX}
+			</tbody>
+		</table>
+	)
+	
+}
+
+export default ContactList;
