4e809fa4d13d3595025e8e637133ed0e563fe2c8
Erno Hentonen <erno.hentonen@opiframe.com>
2024-07-18 16:02:02 +0300

    stage 3

--- react/50_shopping_app/03_stage_login/backend/server.js
+++ react/50_shopping_app/03_stage_login/backend/server.js
@@ -136,7 +136,19 @@ app.post("/login", function(req,res) {
 	});
 })
 
-app.use("/api",shoppingRoute);
+app.post("/logout",function(req,res) {
+	if(!req.headers.token) {
+		return res.status(404).json({"Message":"Not found"})
+	}
+	sessionModel.deleteOne({"token":req.headers.token}).then(function() {
+		return res.status(200).json({"Message":"Success"})
+	}).catch(function(err) {
+		console.log("Failed to remove logout session with token "+req.headers.token+". Reason",err)
+		return res.status(500).json({"Message":"Internal Server Error"});
+	})
+})
+
+app.use("/api",isUserLogged,shoppingRoute);
 
 console.log("Running in port 3000");
 
