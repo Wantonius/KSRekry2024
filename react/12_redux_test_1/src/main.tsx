bdaeb189cde3dda501897d695ffef30bd9510538
Erno Hentonen <erno.hentonen@opiframe.com>
2024-07-23 10:07:00 +0300

    redux test 1

---
+++ react/12_redux_test_1/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import countReducer,{State} from './reducers/countReducer';
+import {Provider} from 'react-redux';
+import {Store,AnyAction,createStore} from 'redux';
+
+const store:Store<State,AnyAction> = createStore(countReducer);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  </React.StrictMode>,
+)
