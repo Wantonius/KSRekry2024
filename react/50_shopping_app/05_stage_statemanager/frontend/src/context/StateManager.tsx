4950356d7d35682052aa686539b53b6c0050f0d5
Erno Hentonen <erno.hentonen@opiframe.com>
2024-07-25 09:59:37 +0300

    shopping app context

---
+++ react/50_shopping_app/05_stage_statemanager/frontend/src/context/StateManager.tsx
@@ -0,0 +1,52 @@
+import React,{useReducer} from 'react';
+import ActionContext from './ActionContext';
+import AppStateContext from './AppStateContext';
+import {AppState} from '../types/states';
+import ShoppingItem from '../models/ShoppingItem';
+import * as actionConstants from '../types/actionConstants';
+import Action from '../types/Action';
+
+interface Props {
+	children:React.ReactNode;re
+}
+
+const getInitialState = ():AppState => {
+	let state = sessionStorage.getItem("state");
+	if(state) {
+		return JSON.parse(state);
+	} else {
+		return {
+			list:[],
+			isLogged:false,
+			token:"",
+			loading:false,
+			error:"",
+			user:""
+		}
+	}
+}
+
+const saveToStorage = (state:AppState) => {
+	sessionStorage.setItem("state",JSON.stringify(state));
+}
+
+const initialState = getInitialState();
+
+const listReducer = (state:AppState,action:Action) => {
+	
+}
+
+const StateManager = (props:Props) => {
+	
+	const [state,dispatch] = useReducer(listReducer,initialState);
+	
+	return(
+		<AppStateContext.Provider value={state}>
+			<ActionContext.Provider value={{dispatch:dispatch}}>
+				{props.children}
+			</ActionContext.Provider>
+		</AppStateContext.Provider>
+	)
+}
+
+export default StateManager;
